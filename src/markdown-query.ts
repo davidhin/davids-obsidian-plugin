65dde5c96b75533a7cf1bb55cea46210bebbe349
David Hin <david.hin@lifelenz.com>
2022-08-18 10:41:54 +0930

    Update

--- src/markdown-query.ts
+++ src/markdown-query.ts
@@ -64,13 +64,19 @@ export async function hello(): Promise<number> {
 	let filteredFiles = files.filter((file) => {
 		if (
 			!file.path.includes("002_Projects") &&
-			!file.path.includes("005_Personal")
+			!file.path.includes("003_Personal")
 		)
 			return false;
 		const fileCache = cache.getFileCache(file);
 		const links = new Set(fileCache["links"]?.map((link) => link.link));
 		const tags = new Set(fileCache["tags"]?.map((tags) => tags.tag));
-		if (!links.has(currFilename) && !tags.has("#wip")) return false;
+		if (
+			file.basename === "Index" ||
+			(!links.has(currFilename) &&
+				!tags.has("#wip") &&
+				!tags.has("#waiting"))
+		)
+			return false;
 		return true;
 	});
 
